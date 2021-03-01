41a60138daa740c9270f4fc6c2890a575ace511f
Danispaz1 <doanmrose@gmail.com>
2021-03-01 12:57:32 -0500

    March 1st

--- java-environment/postgre-example/src/main/java/com/revature/joseph/dao/EmployeeDAO.java
+++ java-environment/postgre-example/src/main/java/com/revature/joseph/dao/EmployeeDAO.java
@@ -1,4 +1,4 @@
-package com.revature.joseph.dao;
+package doanbanking.dao;
 
 import java.util.List;
 
@@ -17,8 +17,14 @@ import com.revature.joseph.model.Employee;
 public interface EmployeeDAO {
 	public Employee selectEmployee(Integer id);
 	public Employee selectEmployee(String name);
+	
 	public Boolean insertIntoEmployees(Employee emp);
 	public List<Employee> selectAllEmployees();
 	public Boolean removeEmployee(Integer id);
 	public Boolean updateEmployee(Employee emp);
 }
+
+		/*emp_id INTEGER PRIMARY KEY, -- ID will be used to identify employees
+		emp_name VARCHAR(50), -- VARCHAR is typically used for string data
+		emp_salary DECIMAL,
+		emp_title VARCHAR(20)*/
